2642332a12297f1192482288f5bdd99c3fcf62f6
lorex03 <lorenaflorio03@gmail.com>
2023-11-24 17:41:42 -0300

    ruta porfolio en pack, pages

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/lorex03/home.git";
+const repoURL = "https://github.com/lorex03/porfolio.git";
 
 ghpages.publish(
   pathname,
